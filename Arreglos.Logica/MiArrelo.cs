dfe4e42aebef969f9fa551003eaad358d96a24a9
AlexTroy01 <25030262@itesa.edu.mx>
2026-09-23 15:01:52 -0600

    version_3: arreglo ascedente y decedente

--- Arreglos.Logica/MiArrelo.cs
+++ Arreglos.Logica/MiArrelo.cs
@@ -6,5 +6,47 @@ namespace Arreglos.Logica
 {
     public class MiArrelo
     {
+        //Atributos o campos
+
+        private int _tope;
+        private int[] _arreglo;
+
+        //constructor
+
+        public MiArrelo(int n) 
+        {
+            N= n;
+            _arreglo = new int[N];
+            _tope = 0;
+        }
+
+        //Propiedades
+
+        public int N { get; }
+        public bool EstaVacio => _tope == 0;
+        public bool EstaLleno => _tope == N;
+        //Metodos
+
+        public void Llenar(int minimo, int maximo)
+        {
+            Random oRandom = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = oRandom.Next(minimo, maximo);
+            }
+
+            _tope = N;
+        }
+
+        public override string ToString()
+        {
+
+            string salida = string.Empty;
+            for (int i = 0; i < _tope; i++)
+            {
+                salida += $"{_arreglo[i]}\t";
+            }
+            return salida;
+        }
     }
 }
--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -4,6 +4,11 @@
     {
         Console.WriteLine("\nArreglo");
 
+        MiArrelo oMiArreglo = new MiArrelo(20);
+
+        oMiArreglo.Llenar(5,20)
+
+        Console.WriteLine(oMiarrelo.ToString());
 
         Console.ReadKey();
     }
